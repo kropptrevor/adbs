5db832103d95585e06bdee0a73f26154f1cbab9e
Trevor Kropp <tkdestroyer1@gmail.com>
2016-10-08 16:55:05 -0400

    feat: add a lower bounds function
    added a lower bounds function similar to the upper bounds function

--- adbs.go
+++ adbs.go
@@ -16,3 +16,17 @@ func CalcUpper(lower float64, expr string, k float64) float64 {
 	}
 	return lower
 }
+
+func CalcLower(upper float64, expr string, k float64) float64 {
+	f := calculus.NewFunc(expr)
+	v := float64(0)
+	for lowr := upper; v != k; lowr -= float64(0.000001) {
+		v = f.AntiDiff(lowr, upper)
+		if v-k < 0.000001 {
+			return lowr
+		} else if lowr == lowr+float64(0.000001) {
+			break
+		}
+	}
+	return upper
+}
