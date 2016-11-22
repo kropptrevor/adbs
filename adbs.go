ecfeed473843dd4340b8ff78b00f47fd7c5f5c6f
Trevor Kropp <tkdestroyer1@gmail.com>
2016-11-22 12:52:14 -0500

    feat: implement timeouts of 1 second
    Added timeout structure to the two functions so that the function doesn't stall.

--- adbs.go
+++ adbs.go
@@ -2,33 +2,61 @@ package adbs
 
 import (
 	calculus "github.com/TheDemx27/calculus"
+	"time"
 	//"math"
 )
 
 func CalcUpper(lower float64, expr string, k float64) float64 {
-	f := calculus.NewFunc(expr)
-	v := float64(0)
-	for uppr := lower; v != k; uppr += float64(0.000001) {
-		v = f.AntiDiff(lower, uppr)
-		if v-k < 0.000001 {
-			return uppr
-		} else if uppr == uppr+float64(0.000001) {
-			break
+	c := make(chan float64, 1)
+	//ret := make(chan float64)
+	go func() {
+		f := calculus.NewFunc(expr)
+		v := float64(0)
+		for uppr := lower; v != k; uppr += float64(0.000001) {
+			v = f.AntiDiff(lower, uppr)
+			if v-k < 0.000001 {
+				//ret <- uppr
+				c <- uppr
+				return
+			} else if uppr == uppr+float64(0.000001) {
+				break
+			}
 		}
+		//ret <- lower
+		c <- lower
+		return
+	}()
+	select {
+	case ret := <-c:
+		return ret
+		//return <-ret
+	case <-time.After(time.Second * 1):
+		return lower
 	}
-	return lower
 }
 
 func CalcLower(upper float64, expr string, k float64) float64 {
-	f := calculus.NewFunc(expr)
-	v := float64(0)
-	for lowr := upper; v != k; lowr -= float64(0.000001) {
-		v = f.AntiDiff(lowr, upper)
-		if v-k < 0.000001 {
-			return lowr
-		} else if lowr == lowr+float64(0.000001) {
-			break
+	c := make(chan float64, 1)
+	go func() {
+		f := calculus.NewFunc(expr)
+		v := float64(0)
+		for lowr := upper; v != k; lowr -= float64(0.000001) {
+			v = f.AntiDiff(lowr, upper)
+			if v-k < 0.000001 {
+				c <- lowr
+				return
+			} else if lowr/(lowr+float64(000001)) < 0.0001 {
+				break
+			}
 		}
+		c <- upper
+		return
+	}()
+	select {
+	case ret := <-c:
+		return ret
+		//return <-ret
+	case <-time.After(time.Second * 1):
+		return upper
 	}
-	return upper
 }
